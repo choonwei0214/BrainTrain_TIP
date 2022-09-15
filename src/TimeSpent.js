0addc53f7629ffa3f58c70cd1a0d39a58e0115fe
Isaac <isaac@surprisedpikachu.tk>
2022-09-15 17:14:57 +0800

    fixed pie chart legend

--- src/TimeSpent.js
+++ src/TimeSpent.js
@@ -1,48 +1,51 @@
-import { Pie } from '@ant-design/plots';
+import { Pie } from "@ant-design/plots";
 
 const TimeSpentChart = () => {
-    const data = [
-        {
-          type: 'SLS (Productivity)',
-          value: 25,
-        },
-        {
-          type: 'Youtube (Entertainment)',
-          value: 13,
-        },
-        {
-          type: 'Google Chrome (News and Information)',
-          value: 11,
-        },
-        {
-          type: 'Others',
-          value: 7,
-        },
-        {
-          type: 'Google Classroom (Productivity)',
-          value: 18,
-        },
-      ];
-      const config = {
-        autoFit: false,
-        appendPadding: 10,
-        data,
-        angleField: 'value',
-        colorField: 'type',
-        radius: 0.8,
-        label: {
-          type: 'outer',
-        },
-        interactions: [
-          {
-            type: 'element-active',
-          },
-        ],
-      };
+  const data = [
+    {
+      type: "SLS (Productivity)",
+      value: 25,
+    },
+    {
+      type: "Youtube (Entertainment)",
+      value: 13,
+    },
+    {
+      type: "Google Chrome (News and Information)",
+      value: 11,
+    },
+    {
+      type: "Others",
+      value: 7,
+    },
+    {
+      type: "Google Classroom (Productivity)",
+      value: 18,
+    },
+  ];
+  const config = {
+    autoFit: false,
+    appendPadding: 10,
+    data,
+    angleField: "value",
+    colorField: "type",
+    radius: 0.8,
+    label: {
+      type: "outer",
+    },
+    interactions: [
+      {
+        type: "element-active",
+      },
+    ],
 
-    return ( 
-        <Pie {...config} />
-     );
-}
- 
+    legend: {
+      layout: "vertical",
+      position: "bottom",
+    },
+  };
+
+  return <Pie {...config} />;
+};
+
 export default TimeSpentChart;
